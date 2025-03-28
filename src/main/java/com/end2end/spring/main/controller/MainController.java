45384ee7e211c7a9f7860c9065d97426c453c43d
qhfka74 <shj04367@gmail.com>
2025-03-28 17:02:01 +0900

    qhfka_v1

--- src/main/java/com/end2end/spring/main/controller/MainController.java
+++ src/main/java/com/end2end/spring/main/controller/MainController.java
@@ -1,13 +1,20 @@
 package com.end2end.spring.main.controller;
 
+import com.end2end.spring.main.dto.ExamDTO;
+import com.end2end.spring.main.service.MainService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
+	@Autowired
+	private MainService mainService;
 	
 	@GetMapping("/")
-	public String home() {
+	public String home(ExamDTO dto) throws Exception {
+		mainService.insertByMember(dto);
 		return "main/index";
 	}
 }

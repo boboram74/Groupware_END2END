a9204635352b717f3a5acade3cca865bbab1d0e9
MoondaeGI <ansgur119977@gmail.com>
2025-03-30 23:43:41 +0900

    add CommuteScheduler -v2

--- src/main/java/com/end2end/spring/commute/scheduler/CommuteScheduler.java
+++ src/main/java/com/end2end/spring/commute/scheduler/CommuteScheduler.java
@@ -1,23 +1,26 @@
 package com.end2end.spring.commute.scheduler;
 
 import com.end2end.spring.util.Statics;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 @Component
 public class CommuteScheduler {
-    @Autowired
-    private RestTemplate restTemplate;
-
     @Scheduled(cron= Statics.WORK_ON_CHECK_TIME)
     public void workOnCheck() {
+        // TODO: 지정한 시간에 자동으로 출근 체크
         System.out.println("출근 체크!!");
     }
 
     @Scheduled(cron=Statics.WORK_OFF_CHECK_TIME)
     public void workOffCheck() {
+        // TODO: 지정한 시간에 자동으로 퇴근 체크
         System.out.println("퇴근 체크!!");
+    }
+
+    @Scheduled(cron="0 0 0 * * 1-5")
+    public void workOffCheckFinal() {
+        // TODO: 연장 근무를 포함한 전인원 퇴근 체크
+        System.out.println("찐막 퇴근 체크!!!");
     }
 }

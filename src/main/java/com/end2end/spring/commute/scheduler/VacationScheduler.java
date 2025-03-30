a494b73b0dd49db5ae3ff7c3057c1e4bde240e1b
MoondaeGI <ansgur119977@gmail.com>
2025-03-31 00:09:17 +0900

    add CommuteScheduler -v3

--- src/main/java/com/end2end/spring/commute/scheduler/VacationScheduler.java
+++ src/main/java/com/end2end/spring/commute/scheduler/VacationScheduler.java
@@ -1,13 +1,16 @@
 package com.end2end.spring.commute.scheduler;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Component
 public class VacationScheduler {
     @Scheduled(cron= "0 0 0 1 * ?")
     public void workOnCheck() {
         // TODO: 매달 연차 지급
-        System.out.println("휴가 체크!!");
+        System.out.println("휴가 체크!! time:" + LocalDateTime.now());
     }
 }

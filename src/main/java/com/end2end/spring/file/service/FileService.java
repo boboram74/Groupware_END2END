3451ec1df2f3e129e89a7a9a9d59f4e350ce46a7
lgh <dlarmsgks@naver.com>
2025-04-02 19:28:02 +0900

    lgh/boarddetail/boardlist

--- src/main/java/com/end2end/spring/file/service/FileService.java
+++ src/main/java/com/end2end/spring/file/service/FileService.java
@@ -4,8 +4,10 @@ import com.end2end.spring.file.dto.FileDTO;
 import com.end2end.spring.file.dto.FileDetailDTO;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface FileService {
     List<FileDetailDTO> selectByParentsId(FileDTO dto);
+    void deleteById(int id) throws IOException;
 }

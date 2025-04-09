0658b5c79fad4b57a731e8f8df30879db96bb8a7
ming <alswl5216@gmail.com>
2025-04-09 14:18:04 +0900

    직원등록v4_승인진행중

--- src/main/webapp/resources/static/js/hr/list.js
+++ src/main/webapp/resources/static/js/hr/list.js
@@ -2,8 +2,10 @@ $("#searchBtn").on("click",function(){
     alert("검색버튼 클릭");
 })
 $("#approveBtn").on("click",function(){
-    location.href="/hr/roleUpdate";
+    let empId = $(this).data("id");
+    location.href="/hr/roleUpdate?id="+ empId;
 })
 $("#rejectBtn").on("click",function(){
-    alert("반려버튼 클릭");
+    let empId = $(this).data("id");
+    alert("반려 버튼 클릭, employee id: " + empId);
 })

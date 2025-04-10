daa044811d1144f8d667fc09d7fa6419e641ae73
yoonah <rkddbsdk3@naver.com>
2025-04-10 20:28:35 +0900

    works 상세보기 기능

--- src/main/java/com/end2end/spring/works/service/ProjectWorkService.java
+++ src/main/java/com/end2end/spring/works/service/ProjectWorkService.java
@@ -1,14 +1,17 @@
 package com.end2end.spring.works.service;
 
 import com.end2end.spring.works.dto.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface ProjectWorkService {
     List<ProjectWorkDTO> selectAll();
-    void insert(ProjectWorkDTO dto);
+    void insert(MultipartFile[] files, ProjectWorkDTO dto) throws Exception;
     void update(ProjectWorkDTO dto);
     void deleteById(int id);
     void toggleState(int id);
     String selectByProjectIdAndEmployeeId(int projectId, String employeeId);
+
+    ProjectWorkDTO selectByworksId(int id);
 }

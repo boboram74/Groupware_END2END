47a2cc1066333d33d2240c71741aa65adb873525
MoondaeGI <ansgur119977@gmail.com>
2025-04-15 00:39:39 +0900

    delete unused classes

--- src/main/java/com/end2end/spring/util/Statics.java
+++ src/main/java/com/end2end/spring/util/Statics.java
@@ -10,9 +10,12 @@ public class Statics {
     public static final String MAPPED_FILE_UPLOAD_PATH = "/files/";
 
     //페이지 유틸
-    public static int recordCountPerPage = 10;
-    public static int naaviCountPerPage = 10;
+    public static final int recordCountPerPage = 10;
+    public static final int naaviCountPerPage = 10;
 
     // API 서비스키
-    public static String apiServiceKey =  "NWIrVZ45vQRGOrgG1lzJk3edmMTejzwM6zNqwKU8dfW8dfsoyWiQ9pKZi0HfpkxbUxqyLGFSVJM8SESuqt1L5w%3D%3D";
+    public static final String apiServiceKey =  "NWIrVZ45vQRGOrgG1lzJk3edmMTejzwM6zNqwKU8dfW8dfsoyWiQ9pKZi0HfpkxbUxqyLGFSVJM8SESuqt1L5w%3D%3D";
+
+    // 설정된 회사 IP
+    public static final String COMPANY_IP = "";
 }

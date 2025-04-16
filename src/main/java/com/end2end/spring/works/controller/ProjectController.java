1f8c90203b8c468a948e97bf1b98a2cae150b7e4
yoonah <rkddbsdk3@naver.com>
2025-04-16 09:10:55 +0900

    저장후 풀땡길거

--- src/main/java/com/end2end/spring/works/controller/ProjectController.java
+++ src/main/java/com/end2end/spring/works/controller/ProjectController.java
@@ -132,7 +132,7 @@ public class ProjectController {
         projectService.updateProjectUser(projectId, employeeId);
 
 
-        return "redirect:/project/main";
+        return "redirect:/project/detail/" + projectId;
     }
 
     // Delete

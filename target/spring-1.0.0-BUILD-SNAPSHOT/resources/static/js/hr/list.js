b04b5c9f9ae203ede46653f2e45d2711e9cf2868
test <test@test.com>
2025-04-17 15:11:06 +0900

    get pull request

--- target/spring-1.0.0-BUILD-SNAPSHOT/resources/static/js/hr/list.js
+++ target/spring-1.0.0-BUILD-SNAPSHOT/resources/static/js/hr/list.js
@@ -1,8 +1,4 @@
 $(document).ready(function(){
-    $("#searchBtn").on("click",function(){
-        alert("검색버튼 클릭");
-    })
-
     $(".approveBtn").on("click",function(){
         let id = $(this).data("id");
         location.href="/hr/roleUpdate/" + id;
